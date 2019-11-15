528a94122f1140cded3ea402d41db8b376344776
Álvaro Cebrián <alvaro.cebrian.garcia@gmail.com>
2019-11-15 13:48:41 +0100

    [BLOG-10] Las rutas del admin son expuestas en json en una ruta incorrecta

--- app/admin/symfony/assets/js/JSRouting.js
+++ app/admin/symfony/assets/js/JSRouting.js
@@ -2,7 +2,7 @@
 
 'use strict';
 
-const routes = require('../../public/assets/js/fos_js_routes.json');
+const routes = require('../../public/build/fos_js_routes.json');
 const Routing =  require('../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js');
 
 Routing.setRoutingData(routes);

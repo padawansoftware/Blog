854e562cf64ae6d56da973255985908bfd62767b
Álvaro Cebrián <alvaro.cebrian.garcia@gmail.com>
2020-05-19 17:22:54 +0200

    [BLOG-52] Improve app 404 page
    App 404 page is ugly and does not adapt to the selected theme. Refactor to adapt to the current theme.

--- app/frontend/vue/src/views/404.vue
+++ app/frontend/vue/src/views/404.vue
@@ -1,18 +1,38 @@
 <template>
     <div id="container">
-        <h1>This is not the page you're looking for</h1>
-        <img src="@/assets/images/droids.png">
+        <h1>Esta no es la página que estás buscando</h1>
+        <img :src="this.img">
     </div>
 </template>
 
 <script>
+    import State from '@/state.js';
+
     export default {
+        data() {
+            return {
+                state: State,
+                img: null
+            };
+        },
         mounted()
         {
+            this.img = this.getImg();
             this.$emit('loaded');
+        },
+        watch: {
+            "state.theme": function() {
+                this.img = this.getImg();
+            }
+        },
+        methods: {
+            getImg() {
+                return require('@/assets/images/droids-' + (this.state.theme.name == 'aliance' ? 'white' : 'black') + '.png');
+            }
         }
     }
 </script>
+
 <style scoped>
     #container {
         text-align: center;
@@ -21,6 +41,11 @@
     h1 {
         font-family: "Star Wars";
         font-size: 40px;
-        color: var(--yellow);
+        color: var(--primary-color);
+    }
+
+    img {
+        max-height: 60vh;
+        background-color: var(--primary-color);
     }
 </style>

f4b1926c957f6a7704b2b07d58d3390dadeef2c8
Álvaro Cebrián <alvaro.cebrian.garcia@gmail.com>
2020-11-16 17:28:17 +0100

    [BLOG-40] Remove JQuery as global variable in admin

--- app/admin/symfony/assets/js/Collection/form.js
+++ app/admin/symfony/assets/js/Collection/form.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const $ = require('jquery');
+
 // Load image uploader plugin
 require('@js/imageUploader.js')
 
--- app/admin/symfony/assets/js/ajax.js
+++ app/admin/symfony/assets/js/ajax.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const $ = require('jquery');
+
 module.exports = {
     delete(deleteUrl, successFunction, errorFunction) {
         $.ajax({
